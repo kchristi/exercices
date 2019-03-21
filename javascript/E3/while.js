4a13e09f814964fa6d637ba72ffb0cd1cff73864
Kevin Christine <kevin.christine.64@gmail.com>
2019-03-21 21:10:08 +0100

    While Loop Exercice
    Section 11 session 134

---
+++ javascript/E3/while.js
@@ -0,0 +1,27 @@
+console.log("1. Print all number between -10 and 19");
+var number = -10;
+while (number <= 19) {
+    console.log(number);
+    number++;
+}
+
+console.log("2. Print all even numbers between 10 and 40");
+number = 10;
+while (number <= 40) {
+    if (number % 2 == 0) console.log(number);
+    number++;
+}
+
+console.log("3. Print all odd numbers between 300 and 333");
+number = 300;
+while (number <= 333) {
+    if (number % 2 == 1) console.log(number);
+    number++;
+}
+
+console.log("4. Print all numbers divisible by 5 and 3 between 5 and 50");
+number = 5;
+while (number <= 50) {
+    if (number % 5 == 0 && number % 3 == 0) console.log(number);
+    number++;
+}
